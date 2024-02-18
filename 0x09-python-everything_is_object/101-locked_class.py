beb83526b5d1ca482800eca2f7f41df330656d45
Abner261 <santosabner949@gmail.com>
2024-02-18 02:33:05 -0800

    class LockedClass with no class or object attribute

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A class with no class or obj attr...
+
+that prevents user: from creating new instance attribute
+
+except: if the new instance attribute is called first_name
+
+"""
+
+
+class LockedClass:
+    """ A class that prevent instance and only allow instance
+    with the attribute: first_name
+    """
+    __slots__ = ["first_name"]
