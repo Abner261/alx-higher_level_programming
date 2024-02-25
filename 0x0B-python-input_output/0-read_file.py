beef2be99311efd0fdeb93a867997af20a8eec1e
Abner261 <santosabner949@gmail.com>
2024-02-25 11:59:47 -0800

    A function that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" A function that reads a text file UTF-8 and prints to standard out """
+""" A function that reads a text file UTF-8 and prints to standard out"""
+
 
 def read_file(filename=""):
-    """ prints the contents of UTF8 text file to stdout """
+    """  The function defination """
     with open(filename, mode='r', encoding='utf-8') as a_file:
         print(a_file.read(), end='')
