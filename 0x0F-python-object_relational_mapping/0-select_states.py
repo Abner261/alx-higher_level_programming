4339fa79da03cf6b76d4756330a539a5e66bbf74
Abner261 <santosabner949@gmail.com>
2024-04-27 14:11:51 -0700

    A script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
+
+"""
+script that lists all states from the database hbtn_0e_0_usa:
+Your script should take 3 arguments: mysql username, mysql password
+and database name (no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    # Check if the correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database_name".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Retrieve the arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Connect to MySQL server
+if __name__ == '__main__':
+    # Establishing a secure connection to the MySQL server
     db = MySQLdb.connect(
-        host="localhost",
+        host='localhost',
         port=3306,
-        user=username,
-        passwd=password,
-        db=database_name
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
     )
 
-    # Create a cursor object
+    # creating a cursor object to execute SQL queries
     cursor = db.cursor()
 
     # Execute the query to select all states sorted by id
