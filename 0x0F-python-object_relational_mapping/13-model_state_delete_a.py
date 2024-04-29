c38b3565991f0a80feda83ce665ad3c2d3d4d985
Abner261 <santosabner949@gmail.com>
2024-04-29 07:33:55 -0700

    A script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python3
-"""Script to delete all State objects with a name containing the letter 'a'
+
+"""
+A Script to delete all State objects with a name containing the letter 'a'
 from the database hbtn_0e_6_usa
 """
 
-import sys
+from sys import argv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    # Check if the number of arguments is correct
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(argv[0]))
+        exit()
 
-    # Create connection to MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database),
-                           pool_pre_ping=True)
+    # Creating the connection string (engine for database)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    # Create session
+    # Creating an instance of Session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query and delete State objects containing 'a' in their names
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    # Querying the State Object for states that contain the letter a
+    states_to_delete = session.query(State).filter(
+        State.name.like('%a%')).all()
+
+    # deleting the State Objects
     for state in states_to_delete:
         session.delete(state)
+
+    # commit the changes
     session.commit()
 
-    print("{} states deleted successfully.".format(len(states_to_delete)))
-
+    # Closing the session
     session.close()
