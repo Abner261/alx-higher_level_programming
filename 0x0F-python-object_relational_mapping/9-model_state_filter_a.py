eb72ce8314f416539c87eb0585d88a21fe404062
Abner261 <santosabner949@gmail.com>
2024-04-29 05:06:20 -0700

    A script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
-"""A Script to list all State objects that contain the letter 'a'
-   from the database hbtn_0e_6_usa
+
+"""
+A Script to list all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa
 """
 
-import sys
+from sys import argv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    # Check if the number of arguments is correct
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(argv[0]))
+        exit()
 
-    # Create connection to MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database),
-                           pool_pre_ping=True)
+    # Creating the connection string (engine for database)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    # Create session
+    # Creating an instance of Session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query and display State objects containing 'a'
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
-    if states_with_a:
-        for state in states_with_a:
-            print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
+    # Querying State Objects that contain the letter `a`
+    filtered_states = session.query(State).filter(State.name.like(
+        '%a%')).order_by(State.id).all()
 
+    # Displaying the results
+    for state in filtered_states:
+        print("{:d}: {:s}".format(state.id, state.name))
+
+    # Closing the session
     session.close()
