3dd6f96d0378ead26d6f0634f26a092dc3c140c4
Abner261 <santosabner949@gmail.com>
2024-04-30 05:46:00 -0700

    A module the defines the state class

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""Defines the State class"""
 
-import sqlalchemy
+"""
+Module that Defines the State class
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-from relationship_city import Base, City
 
+
+# Creating an instance of declarative_base
 Base = declarative_base()
 
 
 class State(Base):
-    """Represents a state for a MySQL database."""
+    """
+    Class representing the states table.
+    Linked to MySQL table "states"
+    """
+    __tablename__ = 'states'
 
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
-
-
+    cities = relationship("City", backref="state")
