c37f9f7064fea9c4fd28a730b7fa3eba74650e63
Abner261 <santosabner949@gmail.com>
2024-05-12 08:37:38 -0700

    A Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,7 +2,8 @@
 """
 This script takes a URL and an email as command-line arguments
 It encodes the email parameter, creates a POST request with the email parameter
-sends the POST request to the provided URL, and then prints the body of the response
+sends the POST request to the provided URL,
+and then prints the body of the response
 """
 
 import sys
